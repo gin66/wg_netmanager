e6c35391db9b3978944299b0943020500d0e11d1
Jochen Kiemes <jochen@kiemes.de>
2021-12-30 18:34:59 +0100

    initial help text

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Wireguard Network Manager")
+        .version("0.1")
+        .author("Jochen Kiemes <jochen@kiemes.de>")
+        .about("Manages a network of wireguard nodes with no central server.")
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .help("Custom config file in ini-style")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("peer")
+                .short("i")
+                .long("ip")
+                .help("IP of a peer to connect to"),
+        )
+        .arg(
+            Arg::with_name("v")
+                .short("v")
+                .multiple(true)
+                .help("Sets the level of verbosity"),
+        )
+        .arg(
+            Arg::with_name("interface")
+                .help("Sets the wireguard interface")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
 }

cd7297c176e09af2c41549ae1d7ab665d8480e42
Jochen Kiemes <jochen@kiemes.de>
2022-01-06 18:14:03 +0100

    do not use ifcfg for android

--- src/main.rs
+++ src/main.rs
@@ -68,28 +68,32 @@ fn set_up_logging(log_filter: log::LevelFilter) {
 }
 
 fn get_interfaces() -> Vec<IpAddr> {
-    let ifaces = ifcfg::IfCfg::get().expect("could not get interfaces");
-    let mut ip_list: Vec<IpAddr> = vec![];
-    trace!("Interfaces");
-    for iface in ifaces.iter() {
-        for addr in iface.addresses.iter() {
-            use ifcfg::AddressFamily::*;
-            match addr.address_family {
-                IPv4 => {
-                    trace!("{:#?}", addr.address.as_ref().unwrap().ip());
-                    ip_list.push(addr.address.as_ref().unwrap().ip());
+    if cfg!(target_os = "android") {
+        vec![]
+    } else {
+        let ifaces = ifcfg::IfCfg::get().expect("could not get interfaces");
+        let mut ip_list: Vec<IpAddr> = vec![];
+        trace!("Interfaces");
+        for iface in ifaces.iter() {
+            for addr in iface.addresses.iter() {
+                use ifcfg::AddressFamily::*;
+                match addr.address_family {
+                    IPv4 => {
+                        trace!("{:#?}", addr.address.as_ref().unwrap().ip());
+                        ip_list.push(addr.address.as_ref().unwrap().ip());
+                    }
+                    IPv6 => {
+                        trace!("{:#?}", addr.address.as_ref().unwrap().ip());
+                        ip_list.push(addr.address.as_ref().unwrap().ip());
+                    }
+                    _ => {}
                 }
-                IPv6 => {
-                    trace!("{:#?}", addr.address.as_ref().unwrap().ip());
-                    ip_list.push(addr.address.as_ref().unwrap().ip());
-                }
-                _ => {}
             }
         }
+        let ip_list = ip_list.into_iter().filter(|ip| !ip.is_loopback()).collect();
+        debug!("Interfaces: {:#?}", ip_list);
+        ip_list
     }
-    let ip_list = ip_list.into_iter().filter(|ip| !ip.is_loopback()).collect();
-    debug!("Interfaces: {:#?}", ip_list);
-    ip_list
 }
 
 fn main() -> BoxResult<()> {

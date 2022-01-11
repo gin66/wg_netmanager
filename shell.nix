57e5daab24042f27730710aabef7fc6ad163960c
Jochen Kiemes <jochen@kiemes.de>
2022-01-11 17:34:56 +0100

    Improved command handling in wg_dev_linuxkernel.rs

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [ rustc cargo gcc ];
-  buildInputs = with pkgs; [ rustfmt clippy bridge_utils killall ];
+  buildInputs = with pkgs; [ rustfmt clippy bridge_utils killall cargo-watch];
 
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
--- src/wg_dev_linuxkernel.rs
+++ src/wg_dev_linuxkernel.rs
@@ -17,245 +17,171 @@ impl WireguardDeviceLinux {
             device_name: wg_name.into(),
         }
     }
+    fn internal_execute_command(
+        &self,
+        mut args: Vec<&str>,
+        input: Option<&str>,
+    ) -> BoxResult<std::process::Output> {
+        let mut args_with_sudo = vec!["sudo"];
+        args_with_sudo.append(&mut args);
+
+        let stdin_par = if input.is_none() {
+            Stdio::null()
+        } else {
+            Stdio::piped()
+        };
+
+        let child = Command::new(args_with_sudo.remove(0))
+            .args(args_with_sudo)
+            .stdin(stdin_par)
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()?;
+
+        if let Some(input) = input {
+            write!(child.stdin.as_ref().unwrap(), "{}", input)
+                .map_err(|e| format!("write to child in execute_command: {:?}", e))?;
+        }
+
+        let output = child.wait_with_output()?;
+
+        if output.status.success() {
+            Ok(output)
+        } else {
+            #[allow(clippy::try_err)]
+            Err(format!(
+                "process failed wth {}",
+                String::from_utf8_lossy(&output.stderr)
+            ))?
+        }
+    }
+    fn execute_command(
+        &self,
+        args: Vec<&str>,
+        input: Option<&str>,
+    ) -> BoxResult<std::process::Output> {
+        trace!(target: "wireguard", "{:?}", args);
+        self.internal_execute_command(args, input).map_err(|e| {
+            error!(target: "wireguard", "{:?}",e);
+            e
+        })
+    }
     fn update_conf(&self, conf: &str, set_new: bool) -> BoxResult<()> {
+        debug!(target: "wireguard", "Update configuration: {}", conf);
         let wg_cmd = if set_new { "setconf" } else { "syncconf" };
 
-        let output = Command::new("sudo")
-            .arg("mktemp")
-            .arg("/tmp/wg_XXXXXXXXXX")
-            .output()
-            .unwrap();
+        let args = vec!["mktemp", "/tmp/wg_XXXXXXXXXX"];
+        let output = self.execute_command(args, None)?;
         let tmpfname = String::from_utf8_lossy(&output.stdout);
         let fname = tmpfname.trim();
-
-        let mut cmd_tee = Command::new("sudo")
-            .arg("tee")
-            .arg("-a")
-            .arg(&*fname)
-            .stdin(Stdio::piped())
-            .stdout(Stdio::null())
-            .spawn()
-            .map_err(|e| format!("{:?}", e))?;
-
-        write!(cmd_tee.stdin.as_ref().unwrap(), "{}", conf)
-            .map_err(|e| format!("{:?}", e))
-            .unwrap();
-
-        let _result = cmd_tee.wait().unwrap();
-
         trace!(target: "wireguard", "temp file {}", fname);
-        let mut cmd = Command::new("sudo")
-            .arg("wg")
-            .arg(wg_cmd)
-            .arg(&self.device_name)
-            .arg(&*fname)
-            .spawn()
-            .unwrap();
-        let result = cmd.wait().unwrap();
-        debug!(target: "wireguard", "wg {}: {:?}", wg_cmd, result);
 
-        let _output = Command::new("sudo")
-            .arg("rm")
-            .arg(&*fname)
-            .status()
-            .unwrap();
-
-        if result.success() {
-            Ok(())
-        } else {
-            strerror("ERROR")
-        }
+        let _ = self.execute_command(vec!["tee", "-a", &*fname], Some(conf))?;
+        let _ = self.execute_command(vec!["wg", wg_cmd, &self.device_name, &*fname], None)?;
+        let _ = self.execute_command(vec!["rm", &*fname], None)?;
+        Ok(())
     }
 }
 
 impl WireguardDevice for WireguardDeviceLinux {
     fn check_device(&self) -> BoxResult<bool> {
         debug!("Check for device {}", self.device_name);
-        let mut cmd = Command::new("ip")
-            .arg("link")
-            .arg("show")
-            .arg(&self.device_name)
-            .spawn()
-            .unwrap();
-
-        let result = cmd.wait().unwrap();
-
-        Ok(result.success())
+        let result = self.execute_command(vec!["ip", "link", "show", &self.device_name], None);
+        Ok(result.is_ok())
     }
     fn bring_up_device(&self) -> BoxResult<()> {
         debug!("Bring up device");
-        let mut cmd = Command::new("sudo")
-            .arg("ip")
-            .arg("link")
-            .arg("add")
-            .arg(&self.device_name)
-            .arg("type")
-            .arg("wireguard")
-            .spawn()
-            .unwrap();
+        let _ = self.execute_command(
+            vec!["ip", "link", "add", &self.device_name, "type", "wireguard"],
+            None,
+        );
+        debug!("Interface {} created", self.device_name);
 
-        let result = cmd.wait().unwrap();
+        let _ = self.execute_command(vec!["ip", "link", "set", &self.device_name, "up"], None);
+        debug!("Interface {} up", self.device_name);
 
-        if result.success() {
-            debug!("Interface {} created", self.device_name);
-        } else {
-        }
-
-        let mut cmd = Command::new("sudo")
-            .arg("ip")
-            .arg("link")
-            .arg("set")
-            .arg(&self.device_name)
-            .arg("up")
-            .spawn()
-            .unwrap();
-
-        let result = cmd.wait().unwrap();
-
-        if result.success() {
-            debug!("Interface {} up", self.device_name);
-        } else {
-        }
         Ok(())
     }
     fn take_down_device(&self) -> BoxResult<()> {
         debug!("Take down device");
-        let mut cmd = Command::new("sudo")
-            .arg("ip")
-            .arg("link")
-            .arg("del")
-            .arg(&self.device_name)
-            .spawn()
-            .unwrap();
-
-        let result = cmd.wait().unwrap();
-
-        if result.success() {
-            debug!("Interface {} destroyed", self.device_name);
-        } else {
-        }
+        let _ = self.execute_command(vec!["ip", "link", "del", &self.device_name], None);
+        debug!("Interface {} destroyed", self.device_name);
         Ok(())
     }
     fn set_ip(&self, ip: &Ipv4Addr) -> BoxResult<()> {
         debug!("Set IP {}", ip);
-        let mut cmd = Command::new("sudo")
-            .arg("ip")
-            .arg("addr")
-            .arg("add")
-            .arg(format!("{}/24", ip.to_string()))
-            .arg("dev")
-            .arg(&self.device_name)
-            .spawn()
-            .unwrap();
+        let ip_extend = format!("{}/24", ip);
+        let _ = self.execute_command(
+            vec![
+                "ip",
+                "addr",
+                "add",
+                &ip_extend,
+                "dev",
+                &self.device_name,
+                "noprefixroute",
+            ],
+            None,
+        );
 
-        let result = cmd.wait().unwrap();
-
-        if result.success() {
-            debug!("Interface {} set ip", self.device_name);
-        } else {
-        }
-
-        debug!("Remove default route {}/24", ip);
-        let mut cmd = Command::new("sudo")
-            .arg("ip")
-            .arg("route")
-            .arg("del")
-            .arg(format!("{}/24", ip.to_string()))
-            .arg("dev")
-            .arg(&self.device_name)
-            .spawn()
-            .unwrap();
-
-        let result = cmd.wait().unwrap();
-
-        if result.success() {
-            debug!("Interface {} set ip", self.device_name);
-        } else {
-        }
+        debug!("Interface {} set ip", self.device_name);
         Ok(())
     }
     fn add_route(&self, route: &str, gateway: Option<Ipv4Addr>) -> BoxResult<()> {
         debug!("Set route {}", route);
-        let mut cmd = if let Some(gateway) = gateway {
-            Command::new("sudo")
-                .arg("ip")
-                .arg("route")
-                .arg("add")
-                .arg(route)
-                .arg("via")
-                .arg(gateway.to_string())
-                .arg("dev")
-                .arg(&self.device_name)
-                .spawn()
-                .unwrap()
+        if let Some(gateway) = gateway {
+            let _ = self.execute_command(
+                vec![
+                    "ip",
+                    "route",
+                    "add",
+                    route,
+                    "via",
+                    &gateway.to_string(),
+                    "dev",
+                    &self.device_name,
+                ],
+                None,
+            );
         } else {
-            Command::new("sudo")
-                .arg("ip")
-                .arg("route")
-                .arg("add")
-                .arg(route)
-                .arg("dev")
-                .arg(&self.device_name)
-                .spawn()
-                .unwrap()
-        };
-
-        let result = cmd.wait().unwrap();
-
-        if result.success() {
-            debug!("Interface {} set route", self.device_name);
-        } else {
+            let _ = self.execute_command(
+                vec!["ip", "route", "add", route, "dev", &self.device_name],
+                None,
+            );
         }
+        debug!("Interface {} set route", self.device_name);
         Ok(())
     }
     fn del_route(&self, route: &str, gateway: Option<Ipv4Addr>) -> BoxResult<()> {
         debug!("Set route {}", route);
-        let mut cmd = if let Some(gateway) = gateway {
-            Command::new("sudo")
-                .arg("ip")
-                .arg("route")
-                .arg("del")
-                .arg(route)
-                .arg("via")
-                .arg(gateway.to_string())
-                .spawn()
-                .unwrap()
+        if let Some(gateway) = gateway {
+            let _ = self.execute_command(
+                vec![
+                    "ip",
+                    "route",
+                    "del",
+                    route,
+                    "via",
+                    &gateway.to_string(),
+                    "dev",
+                    &self.device_name,
+                ],
+                None,
+            );
         } else {
-            Command::new("sudo")
-                .arg("ip")
-                .arg("route")
-                .arg("del")
-                .arg(route)
-                .spawn()
-                .unwrap()
-        };
-
-        let result = cmd.wait().unwrap();
-
-        if result.success() {
-            debug!("Interface {} set route", self.device_name);
-        } else {
+            let _ = self.execute_command(
+                vec!["ip", "route", "del", route, "dev", &self.device_name],
+                None,
+            );
         }
+        debug!("Interface {} deleted route", self.device_name);
         Ok(())
     }
     fn flush_all(&self) -> BoxResult<()> {
         for what in ["route", "addr"] {
             debug!("Flush {}", what);
-            let mut cmd = Command::new("sudo")
-                .arg("ip")
-                .arg(what)
-                .arg("flush")
-                .arg("dev")
-                .arg(&self.device_name)
-                .spawn()
-                .unwrap();
-
-            let result = cmd.wait().unwrap();
-
-            if result.success() {
-                debug!("{} flushed", what);
-            } else {
-            }
+            let _ = self.execute_command(vec!["ip", what, "flush", "dev", &self.device_name], None);
+            debug!("{} flushed", what);
         }
         Ok(())
     }
@@ -267,14 +193,8 @@ impl WireguardDevice for WireguardDeviceLinux {
     }
     fn retrieve_conf(&self) -> BoxResult<HashMap<String, SocketAddr>> {
         let mut pubkey_to_endpoint = HashMap::new();
-        let output = Command::new("sudo")
-            .arg("wg")
-            .arg("showconf")
-            .arg(&self.device_name)
-            .stdout(Stdio::piped())
-            .output()?
-            .stdout;
-        let wg_config = String::from_utf8_lossy(&output);
+        let result = self.execute_command(vec!["wg", "showconf", &self.device_name], None)?;
+        let wg_config = String::from_utf8_lossy(&result.stdout);
         trace!("{}", wg_config);
         let ini = ini::Ini::load_from_str(&wg_config).unwrap();
         for peer_ini in ini.section_all(Some("Peer")) {

3248e812f6b1ca33c2a962fc47ed7175ed0b9ec4
Jochen Kiemes <jochen@kiemes.de>
2022-01-15 19:01:11 +0100

    can get all configuration values from config files

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,38 @@ use std::fs::File;
 use std::io::Read;
 use std::net::Ipv4Addr;
 
-use clap::{App, Arg};
+use clap::{App, Arg, ArgMatches};
 use log::*;
-use yaml_rust::YamlLoader;
+use yaml_rust::{Yaml, YamlLoader};
 
 use wg_netmanager::configuration::*;
 use wg_netmanager::error::*;
 use wg_netmanager::*;
+
+fn get_option_bool(matches: &ArgMatches, config: &Option<Yaml>, option_name: &'static str) -> bool {
+    if matches.is_present(option_name) {
+        return true;
+    }
+
+    if let Some(conf) = config.as_ref() {
+       if let Some(val) = conf[option_name].as_bool() {
+           return val;
+       }
+    }
+
+    false
+}
+fn get_option_string(matches: &ArgMatches, config: &Option<Yaml>, option_name: &'static str) -> BoxResult<String> {
+    if let Some(val) = matches.value_of(option_name) {
+        return Ok(val.to_string());
+    }
+    else if let Some(conf) = config.as_ref() {
+       if let Some(val) = conf[option_name].as_str() {
+           return Ok(val.to_string());
+       }
+    }
+    Err(format!("Configuration option <{}> is not defined", option_name).into())
+}
 
 fn main() -> BoxResult<()> {
     let matches = App::new("Wireguard Network Manager")
@@ -35,9 +60,9 @@ fn main() -> BoxResult<()> {
                 .takes_value(true),
         )
         .arg(
-            Arg::with_name("existing_interface")
+            Arg::with_name("existingInterface")
                 .short("e")
-                .long("existing_wg")
+                .long("existing-wg")
                 .help("Use an existing wireguard interface and do not try to create one"),
         )
         .arg(
@@ -75,7 +100,7 @@ fn main() -> BoxResult<()> {
                 .help("Sets the level of verbosity"),
         )
         .arg(
-            Arg::with_name("interface")
+            Arg::with_name("wgInterface")
                 .short("i")
                 .long("wireguard-interface")
                 .help("Sets the wireguard interface")
@@ -83,7 +108,7 @@ fn main() -> BoxResult<()> {
                 .takes_value(true),
         )
         .arg(
-            Arg::with_name("wg_ip")
+            Arg::with_name("wgIp")
                 .short("a")
                 .long("wireguard-address")
                 .help("Sets the wireguard ip address (ipv4)")
@@ -99,11 +124,24 @@ fn main() -> BoxResult<()> {
         .get_matches();
 
     let use_tui = matches.is_present("tui");
-    let use_existing_interface = matches.is_present("existing_interface");
-    let computer_name = matches.value_of("name").unwrap();
+
+    let mut opt_peer_conf: Option<Yaml> = None;
+    let peer_config = matches.value_of("peer_config").unwrap();
+    if let Ok(mut file) = File::open(peer_config) {
+        let mut content = String::new();
+        file.read_to_string(&mut content)?;
+        let mut peer_conf = YamlLoader::load_from_str(&content).unwrap();
+        if peer_conf.len() != 1 {
+            return Err("Malformed peer configuration".into());
+        }
+        opt_peer_conf = Some(peer_conf.remove(0));
+    }
+
+    let computer_name = get_option_string(&matches, &opt_peer_conf, "name")?;
 
     // Select logger based on command line flag
     //
+    // Cannot initialize earlier, because the computer name is needed
     let opt_fname = if matches.is_present("logfile") {
         Some(format!("{}.log", computer_name))
     } else {
@@ -126,29 +164,28 @@ fn main() -> BoxResult<()> {
         set_up_logging(log_filter, opt_fname)?;
     }
 
-    let interface = matches.value_of("interface").unwrap();
-    let wg_ip: Ipv4Addr = matches.value_of("wg_ip").unwrap().parse().unwrap();
-    let wg_port: u16 = matches.value_of("wireguard_port").unwrap().parse().unwrap();
-    let admin_port: u16 = matches.value_of("admin_port").unwrap().parse().unwrap();
-
-    let ip_list = Arch::get_local_interfaces();
-
     let network_config = matches.value_of("network_config").unwrap();
     let mut file = File::open(network_config)?;
     let mut content = String::new();
     file.read_to_string(&mut content)?;
-    let conf = YamlLoader::load_from_str(&content).unwrap();
-
-    let peer_config = matches.value_of("peer_config").unwrap();
-    let mut file = File::open(peer_config)?;
-    let mut content = String::new();
-    file.read_to_string(&mut content)?;
-    let peer_conf = YamlLoader::load_from_str(&content).unwrap();
-
+    let network_conf_vec = YamlLoader::load_from_str(&content).unwrap();
+    if network_conf_vec.len() != 1 {
+        return Err("Malformed network configuration".into());
+    }
+    let network_conf = &network_conf_vec[0];
     debug!("Raw configuration:");
-    debug!("{:#?}", conf);
+    debug!("{:#?}", network_conf);
 
-    let network = &conf[0]["network"];
+    let ip_list = Arch::get_local_interfaces();
+
+    let use_existing_interface = get_option_bool(&matches, &opt_peer_conf, "existingInterface");
+    let interface = get_option_string(&matches, &opt_peer_conf, "wgInterface")?;
+    let wg_ip_string = get_option_string(&matches, &opt_peer_conf, "wgIp")?;
+    let wg_ip: Ipv4Addr = wg_ip_string.parse().unwrap();
+    let wg_port: u16 = matches.value_of("wireguard_port").unwrap().parse().unwrap();
+    let admin_port: u16 = matches.value_of("admin_port").unwrap().parse().unwrap();
+
+    let network = &network_conf["network"];
     let shared_key = base64::decode(&network["sharedKey"].as_str().unwrap()).unwrap();
     let subnet: ipnet::Ipv4Net = network["subnet"].as_str().unwrap().parse().unwrap();
 
@@ -157,7 +194,7 @@ fn main() -> BoxResult<()> {
     }
 
     let mut peers: HashMap<Ipv4Addr, PublicPeer> = HashMap::new();
-    for p in conf[0]["peers"].as_vec().unwrap() {
+    for p in network_conf["peers"].as_vec().unwrap() {
         info!("STATIC PEER: {:#?}", p);
         let endpoint = p["endPoint"].as_str().unwrap().to_string();
         let mut flds = endpoint.split(':').collect::<Vec<_>>();
@@ -174,7 +211,7 @@ fn main() -> BoxResult<()> {
         peers.insert(wg_ip, pp);
     }
 
-    let wg_dev = Arch::get_wg_dev(interface);
+    let wg_dev = Arch::get_wg_dev(&interface);
     let (my_private_key, my_public_key) = wg_dev.create_key_pair()?;
     trace!("My private key: {}", my_private_key);
     trace!("My public key: {}", my_public_key);

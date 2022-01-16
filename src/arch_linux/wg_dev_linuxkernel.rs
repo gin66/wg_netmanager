80067e07b464b6ae82f2e18cfd7e3b1615b28d78
Jochen Kiemes <jochen@kiemes.de>
2022-01-16 17:01:13 +0100

    cargo fmt

--- src/arch_linux/wg_dev_linuxkernel.rs
+++ src/arch_linux/wg_dev_linuxkernel.rs
@@ -103,7 +103,14 @@ impl WireguardDevice for WireguardDeviceLinux {
             // try wireguard-go
             debug!("Create device via wireguard-go");
             // Setting the environment variable works only with sudo
-            result = self.execute_command(vec!["WG_I_PREFER_BUGGY_USERSPACE_TO_POLISHED_KMOD=1", "wireguard-go", &self.device_name], None);
+            result = self.execute_command(
+                vec![
+                    "WG_I_PREFER_BUGGY_USERSPACE_TO_POLISHED_KMOD=1",
+                    "wireguard-go",
+                    &self.device_name,
+                ],
+                None,
+            );
         }
 
         if result.is_err() {

51ea647c27a1ac4a57d54a56328c74f17eb35399
Jochen Kiemes <jochen@kiemes.de>
2022-01-16 17:11:06 +0100

    bump version

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ fn get_option_string(
 
 fn main() -> BoxResult<()> {
     let matches = App::new("Wireguard Network Manager")
-        .version("0.3.7")
+        .version("0.3.8")
         .author("Jochen Kiemes <jochen@kiemes.de>")
         .about("Manages a network of wireguard nodes with no central server.")
         .arg(

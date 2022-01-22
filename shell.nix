4978c6b4e33091878a4ba7623b52978698f44a5f
Jochen Kiemes <jochen@kiemes.de>
2022-01-22 12:16:13 +0100

    add Notes to shell.nix

--- shell.nix
+++ shell.nix
@@ -5,3 +5,19 @@ pkgs.mkShell {
 
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
+
+#
+# Note for nixos-release:
+#
+# - Package path: pkgs/tools/networking/wg-netmanager
+# - Service path:
+#
+# 1. Update default.nix, update version and replace hashes with 00000.. and 1111.
+# 2. Run `nix-build ~/src/nixpkgs -A wg-netmanager --show-trace`
+# 3. Replace fetchFromGitHub-hash
+# 4. Run `nix-build ~/src/nixpkgs -A wg-netmanager --show-trace`
+# 5. Reokace cargoSha256
+# 6. Run `nix-build ~/src/nixpkgs -A wg-netmanager --show-trace`
+#    => should work now
+# 7. cd to ~/.dotfiles and run `./apply-local-system.sh switch'
+
